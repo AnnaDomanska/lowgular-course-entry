3b7c7ad62bd483b571018615eb2d1747509b376e
annah <anna.h.domanska@gmail.com>
2022-11-18 13:53:41 +0100

    feat: added alert with new employee data after submit employee form

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -13,4 +13,8 @@ export class EmployeeFormComponent {
     age: new FormControl(null, [Validators.required, Validators.min(18)]),
     salary: new FormControl(null, [Validators.required])
   });
+
+  onButtonClicked(form: {name: string, age: number, salary: number}) {
+    alert(`User was successfully added to the database. Name: ${form.name}, age: ${form.age}, salary: ${form.salary}`)
+  }
 }
