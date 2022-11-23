2da5b5b85a4a7ad5b9c5ebad6ec02c6209a06eee
annah <anna.h.domanska@gmail.com>
2022-11-23 12:56:41 +0100

    feat: added router link to employee list, added employee details component

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -3,10 +3,10 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { PersonModel } from '../model/person.model';
+import { ApiResponse } from './api.response';
+import { EmployeeResponse } from './employee.response';
 import { CreateEmployeeModel } from '../model/create-employee.model';
-import { ApiResponse } from "./api.response";
-import { EmployeeResponse} from "./employee.response"
-
+import { EmployeeModel } from '../model/employee.model';
 
 @Injectable()
 export class EmployeeService {
@@ -16,14 +16,14 @@ export class EmployeeService {
   getAll(): Observable<PersonModel[]> {
     return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees').pipe(
       map((response: ApiResponse<EmployeeResponse[]>): PersonModel[] => {
-      return response.data.map((employeeResponse: EmployeeResponse) => {
-        return{
-          name: employeeResponse.employee_name,
-          personalNumber: employeeResponse.id,
-          img: employeeResponse.profile_image,
-          mail: ''
-        }
-      });
+        return response.data.map((employeeResponse: EmployeeResponse) => {
+          return {
+            name: employeeResponse.employee_name,
+            personalNumber: employeeResponse.id,
+            img: employeeResponse.profile_image,
+            mail: ''
+          }
+        });
       }));
   }
 
@@ -32,7 +32,16 @@ export class EmployeeService {
     return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee).pipe(map(_ => void 0))
   }
   delete(id: string): Observable<void> {
-    return this._httpClient.delete('https://dummy.restapiexample.com/api/v1/delete/2' + id).pipe(map(_=> void 0))
+    return this._httpClient.delete('https://dummy.restapiexample.com/api/v1/delete/2' + id).pipe(map(_ => void 0))
   }
 
+  getOne(id: string): Observable<EmployeeModel> {
+    return this._httpClient.get<ApiResponse<EmployeeResponse>>('https://dummy.restapiexample.com/api/v1/employee/' +id).pipe(
+      map((response: ApiResponse<EmployeeResponse>): EmployeeModel => ({
+        id: response.data.id,
+        image: response.data.profile_image,
+        email: '',
+        name: response.data.employee_name
+      }))
+    )  };
 }
